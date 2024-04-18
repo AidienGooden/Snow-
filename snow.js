2e1a6c5b67b0634ed4182ff38ddc39a29da8138c
EquinoxEgo <goodenaidien@Gmail.com>
2024-04-18 10:01:21 -0400

    ..

--- snow.js
+++ snow.js
@@ -25,10 +25,33 @@ function myFunction() {
     let x = document.getElementById("text_1");
     let y = document.getElementById("text_2");
     if (x.style.display === "none") {
-      x.style.display = "block";
-      y.style.display = "none";
+        x.style.display = "block";
+        y.style.display = "none";
     } else {
         x.style.display = "none";
         y.style.display = "block";
     }
-  }
+}
+
+
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id))
+  {js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';
+  fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+
+
+
+
+
+
+
+  document.getElementById("drop").addEventListener("click", function(e) {
+    document.getElementById("myDropdown").classList.toggle("show");
+  });
+  
+  // Close the dropdown if the user clicks outside of it
+  window.addEventListener("click", function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      document.querySelectorAll(".dropdown-content.show")
+        .forEach(openDropdown => openDropdown.classList.remove('show'))
+    } 
+  });
