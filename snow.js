0c8e51de7a4272643f5109d80588da8dee4ec098
EquinoxEgo <goodenaidien@Gmail.com>
2024-04-19 10:37:18 -0400

    Chromebook

--- snow.js
+++ snow.js
@@ -20,7 +20,7 @@ sections.forEach((section) => {
         section.classList.add("active");
     });
 });
-
+//EVENTS
 function myFunction() {
     let x = document.getElementById("text_1");
     let y = document.getElementById("text_2");
@@ -32,8 +32,23 @@ function myFunction() {
         y.style.display = "block";
     }
 }
+let countDownDate = new Date("Sep 1, 2024 12:00:00").getTime();
+let x = setInterval(function(){
+    let now = new Date().getTime();
+    let distance = countDownDate - now;
+    let days = Math.floor(distance/ (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 *60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+    document.getElementById('timer').innerText = days + " : " + hours + " : " + minutes + " : " + seconds;
+    if(distance < 0){
+        clearInterval(x);
+        document.getElementById('timer').innerText = "EXPIRED";
+    }
+}, 1000);
 
+//WEATHER
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id))
   {js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';
   fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
@@ -55,3 +70,13 @@ function myFunction() {
         .forEach(openDropdown => openDropdown.classList.remove('show'))
     } 
   });
+
+  //Empolyment//
+  function ValidateForm(frm) {
+    if (frm.First_Name.value == "") { alert('First name is required.'); frm.First_Name.focus(); return false; }
+    if (frm.Last_Name.value == "") { alert('Last name is required.'); frm.Last_Name.focus(); return false; }
+    if (frm.Email_Address.value == "") { alert('Email address is required.'); frm.Email_Address.focus(); return false; }
+    if (frm.Email_Address.value.indexOf("@") < 1 || frm.Email_Address.value.indexOf(".") < 1) { alert('Please enter a valid email address.'); frm.Email_Address.focus(); return false; }
+    if (frm.Position.value == "") { alert('Position is required.'); frm.Position.focus(); return false; }
+    if (frm.Phone.value == "") { alert('Phone is required.'); frm.Phone.focus(); return false; }
+    return true; }
